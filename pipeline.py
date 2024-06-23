268f782bed92305192f7a00011d20c7c79da78fc
DrLouback <drloubackmateus@gmail.com>
2024-06-23 19:49:13 -0300

    'load_sucedido'

--- pipeline.py
+++ pipeline.py
@@ -7,15 +7,16 @@ from dotenv import load_dotenv
 from pandas import DataFrame
 from duckdb import DuckDBPyRelation
 
+#Aqui estou carregando as variáveis do arquivo .env
 load_dotenv()
 
-
+#Primeiro fazemos o extract dos arquivos que estão na pasta do google drive com gdown
 def baixar_os_arquivos_do_google_drive(url_pasta, diretorio_local):
     os.makedirs(diretorio_local, exist_ok= True)
     gdown.download_folder(url_pasta, output= diretorio_local, quiet=False, use_cookies= False)
 
-# Listar arquivos CSV no diretório especificado
 
+# Fazer a listagem dos arquivos que foram baixados
 def listar_arquivos_csv (diretorio):
     arquivos_csv = []
     todos_os_arquivos = os.listdir(diretorio)
@@ -26,34 +27,61 @@ def listar_arquivos_csv (diretorio):
             
         return arquivos_csv
     
-#Ler um arquivo csv e retornar em um dataframe duckdb
 
+# Ler os arquivos .csv no Duckdb
 def ler_csv (caminho_do_arquivo):
     dataframe_duckdb =  duckdb.read_csv(caminho_do_arquivo)
     
     return dataframe_duckdb
 
+#Fazer o transform do .csv e passar para um dataframe Pandas:
 def transformar(df: DuckDBPyRelation) -> DataFrame: 
     dataframe_transformado = duckdb.sql("Select *, quantidade * valor as total_vendas from df").df()
-    print(dataframe_transformado)
+    
     return dataframe_transformado
 
-def salvar_no_postgres(df_duckdb,tabela):
-    DATABASE_URL = os.getenv('DATABASE_URL') 
-    engine = create_engine(DATABASE_URL) # type: ignore
-    df_duckdb.to_sql(tabela, con=engine, if_exists='append', index= False)
 
+#Fazer o load do dataframe transformado no banco de dados postgres:
+def salvar_no_postgres(df_duckdb, tabela):
+    DATABASE_URL = os.getenv("DATABASE_URL")  # Ex: 'postgresql://user:password@localhost:5432/database_name'
+    if isinstance(DATABASE_URL,str):
+        engine = create_engine(DATABASE_URL) #
+    # Salvar o DataFrame no PostgreSQL
+    df_duckdb.to_sql(tabela, con=engine, if_exists='append', index=False)
 
 
 
 if __name__ == "__main__":
 
-    url_pasta = "https://drive.google.com/drive/folders/1lVNzpYavpcUcEyyaIGwKPsqoFzoN-ZWH?usp"
-    diretorio_local = "./pasta_gdown"
-    baixar_os_arquivos_do_google_drive(url_pasta,diretorio_local)
-      
-    lista_de_arquivos = listar_arquivos_csv(diretorio_local)
-    for caminho_do_arquivo in lista_de_arquivos:# type: ignore
-        duck_db_df = ler_csv(caminho_do_arquivo)
-        pandas_df_transformado =transformar(duck_db_df)
-        salvar_no_postgres(pandas_df_transformado,"vendas_calculado")
+    try:
+        
+
+        url_pasta = 'https://drive.google.com/drive/folders/1lVNzpYavpcUcEyyaIGwKPsqoFzoN-ZWH'
+        diretorio_local = './pasta_gdown'
+
+        if not url_pasta:
+            raise ValueError ('''
+                          A url da pasta do Google não está
+                          definido corretamente no .env''')
+        if not diretorio_local:
+            raise ValueError ('''
+                          O Diretrório local não está
+                          definido corretamente no .env''')
+
+        baixar_os_arquivos_do_google_drive(url_pasta, diretorio_local)
+        listar_arquivos = listar_arquivos_csv(diretorio_local)
+
+        if not listar_arquivos:
+            raise ValueError('A lista de arquivos está vazia')
+        
+        for caminho_arquivos in listar_arquivos:
+            df = ler_csv(caminho_arquivos)
+            df_transformado = transformar(df)
+            salvar_no_postgres(df_transformado, 'vendas_calculadas')
+
+
+    except Exception as e:
+        print(f'Ocorreu um erro {e}')
+
+    
+       
